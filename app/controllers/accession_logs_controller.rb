46077872b3cc622e05ba736b81d108df35464ed8
Prelang Builder <builder@prelang.com>
2014-08-20 00:08:51 +0000

    Added require_user_signed_in filters to scaffolded controller

--- app/controllers/accession_logs_controller.rb
+++ app/controllers/accession_logs_controller.rb
@@ -1,4 +1,8 @@
 class AccessionLogsController < ApplicationController
+
+  #->Prelang (scaffolding:rails/scope_to_user)
+  before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
+
   before_action :set_accession_log, only: [:show, :edit, :update, :destroy]
 
   # GET /accession_logs
